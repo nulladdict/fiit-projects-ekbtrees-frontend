788d26c7664e1f7de8d694b4af6eac988dbccfb4
fritsler.ve@jetstyle.ru <fritsler.ve@jetstyle.ru>
2022-03-29 15:00:18 +0500

    refactor requests.ts

--- src/helpers/requests.ts
+++ src/helpers/requests.ts
@@ -4,129 +4,74 @@ import decode from "jwt-decode";
 const cookies = new Cookies();
 
 export default class RequestService {
-	static getData(url: string, headers: HeadersInit = {}) {
-		return this.refreshToken().then(() => {
-			const token = cookies.get('AccessToken');
-			if (token) {
-				headers = {
-					...headers,
-					'Authorization': `Bearer ${token}`
-				}
-			}
-			return fetch(url, {
-				method: 'GET',
-				headers: headers,
-			})
-				.then(response => {
-					if (response.status !== 200) {
-						throw `${response.status} ${response.statusText}`;
-					}
-
-					return response.json();
-				});
-		})
+	static async processHeaders(headers: HeadersInit = {}) {
+		await this.refreshToken()
+		const token = cookies.get('AccessToken');
+		return {
+			...headers,
+			...token && {'Authorization': `Bearer ${token}`}
+		};
 	}
 
-	// TODO: find а more specific type for body
-	static postData(url: string, body: BodyInit | null | undefined, headers: HeadersInit = {}): Promise<any> {
-		return this.refreshToken().then(() => {
-			const token = cookies.get('AccessToken');
-			if (token) {
-				headers = {
-					...headers,
-					'Authorization': `Bearer ${token}`
-				}
-			}
-			return fetch(url, {
-				method: 'POST',
-				headers: headers,
-				body
-			})
-				.then(response => {
-					const passingStatuses = [200, 201];
+	static async validateAndParseResponse(response: Response) {
+		if (!([200, 201].includes(response.status))) {
+			throw `${response.status} ${response.statusText}`;
+		}
+		return await response.json();
+	}
 
-					if (!passingStatuses.includes(response.status)) {
-						throw `${response.status} ${response.statusText}`;
-					}
-
-					return response.json();
-				});
+	static async getData(url: string, headers: HeadersInit = {}) {
+		const response = await fetch(url, {
+			method: 'GET',
+			headers: await this.processHeaders(headers),
 		});
+		return await this.validateAndParseResponse(response);
 	}
 
-	static putData(url: string, body: BodyInit | null | undefined, headers: HeadersInit = {}): Promise<any> {
-		return this.refreshToken().then(() => {
-			const token = cookies.get('AccessToken');
-			if (token) {
-				headers = {
-					...headers,
-					'Authorization': `Bearer ${token}`
-				}
-			}
-			return fetch(url, {
-				method: 'PUT',
-				headers: headers,
-				body
-			})
-				.then(response => {
-					const passingStatuses = [200, 201];
-
-					if (!passingStatuses.includes(response.status)) {
-						throw `${response.status} ${response.statusText}`;
-					}
-
-					return true;
-				});
+	static async postData(url: string, body: BodyInit | null | undefined, headers: HeadersInit = {}) {
+		const response = await fetch(url, {
+			method: 'POST',
+			headers: await this.processHeaders(headers),
+			body
 		});
+		return await this.validateAndParseResponse(response);
 	}
 
-	static deleteData(url: string, headers: HeadersInit = {}): Promise<boolean> {
-		return this.refreshToken().then(() => {
-			const token = cookies.get('AccessToken');
-			if (token) {
-				headers = {
-					...headers,
-					'Authorization': `Bearer ${token}`
-				}
-			}
-			return fetch(url, {
-				method: 'DELETE',
-				headers: headers,
-			})
-				.then(response => {
-					if (response.status !== 200) {
-						throw `${response.status} ${response.statusText}`;
-						return false;
-					}
-
-					return true;
-				});
+	static async putData(url: string, body: BodyInit | null | undefined, headers: HeadersInit = {}) {
+		const response = await fetch(url, {
+			method: 'PUT',
+			headers: await this.processHeaders(headers),
+			body
 		});
+		await this.validateAndParseResponse(response);
+		return true;
 	}
 
-	static refreshToken (): Promise<any> {
+	static async deleteData(url: string, headers: HeadersInit = {}): Promise<boolean> {
+		let response = await fetch(url, {
+			method: 'DELETE',
+			headers: await this.processHeaders(headers),
+		});
+		return await this.validateAndParseResponse(response);
+	}
+
+	static refreshToken() {
 		const token = cookies.get('AccessToken');
 		const decoded_token: any = token ? decode(token) : null;
 		if(decoded_token && decoded_token.exp - Math.round(Date.now() / 1000) < 10) {
 			return fetch("/auth/newTokens", {
 				method: 'POST',
-				headers: {
-					'Authorization': `Bearer ${token}`
-				},
+				headers: { 'Authorization': `Bearer ${token}` },
 			})
 		}
-		else {
-			return Promise.resolve();
-		}
+		return Promise.resolve();
 	}
 
-	static refreshTokenForce (): Promise<any> {
+	static refreshTokenForce() {
 		const token = cookies.get('AccessToken');
 		return fetch("/auth/newTokens", {
 			method: 'POST',
-			headers: {
-				'Authorization': `Bearer ${token}`
-			},
+			headers: { 'Authorization': `Bearer ${token}` },
 		})
 	}
 }
